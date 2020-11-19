8e1b62aa8ea44e4fb80a5424584a482ab55e5f4b
Andrii Savchenko <andrsvchnko@gmail.com>
2020-11-19 10:32:19 +0200

    Add account creation with username only

--- lib/convabout/accounts/user.ex
+++ lib/convabout/accounts/user.ex
@@ -11,12 +11,29 @@ defmodule Convabout.Accounts.User do
 
   alias Argon2
 
+  @generated_password_length 16
+
   @doc false
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:username, :password])
-    |> validate_required([:username, :password])
+    |> validate_required(:username)
+    |> generate_password_if_blank()
     |> put_password_hash()
+  end
+
+  defp generate_password_if_blank(%Ecto.Changeset{changes: %{password: _}} = changeset) do
+    changeset
+  end
+
+  defp generate_password_if_blank(%Ecto.Changeset{valid?: true} = changeset) do
+    change(changeset, password: random_string(@generated_password_length))
+  end
+
+  defp generate_password_if_blank(changeset), do: changeset
+
+  defp random_string(length) do
+    :crypto.strong_rand_bytes(length) |> Base.encode64() |> binary_part(0, length)
   end
 
   defp put_password_hash(
