4b4630b4353fc7733af9a6b4ee1e9bcf248e87d7
Andrii Savchenko <andrsvchnko@gmail.com>
2020-11-19 10:22:31 +0200

    Add signing in automatically on registration

--- lib/convabout_web/controllers/registration_controller.ex
+++ lib/convabout_web/controllers/registration_controller.ex
@@ -3,21 +3,24 @@ defmodule ConvaboutWeb.RegistrationController do
 
   alias Convabout.Repo
 
-  alias Convabout.Accounts.User
+  alias Convabout.{Accounts.User, Accounts.Guardian}
 
   def sign_up(conn, %{"user" => user_params}) do
     changeset = User.changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
+        {:ok, jwt, _claims} = Guardian.encode_and_sign(user)
+
         conn
         |> put_status(:created)
-        |> render("success.json", user: user)
+        |> render("success.json", user: user, jwt: jwt)
 
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
-        |> render(Convabout.ChangesetView, "error.json", changeset: changeset)
+        |> put_view(ConvaboutWeb.ChangesetView)
+        |> render("error.json", changeset: changeset)
     end
   end
 end
--- lib/convabout_web/views/registration_view.ex
+++ lib/convabout_web/views/registration_view.ex
@@ -1,9 +1,13 @@
 defmodule ConvaboutWeb.RegistrationView do
   use ConvaboutWeb, :view
 
-  def render("success.json", %{user: user}) do
+  def render("success.json", %{user: user, jwt: jwt}) do
     %{
       status: :ok,
+      data: %{
+        token: jwt,
+        username: user.username
+      },
       message: """
         Now you can sign in using your email and password at /api/sign_in. You will receive JWT token.
         Please put this token into Authorization header for all authorized requests.
